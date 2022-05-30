7bde0e447358ac7c984842d41d2c27f792baf5eb
KCH6937 <adasddasd12@naver.com>
2022-05-30 23:05:37 +0900

    코드 정리, morgan 라이브러리, 에러 미들웨어 추가

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-const cors = require("cors"); // 웹 페이지의 스크립트가 그 페이지와 같은 서버에 있는 주소로만 ajax 요청을 할 수 있도록 처리하는 경우를 해제 해주는 기능
-const session = require("express-session");
+const express = require('express');
+const cors = require('cors'); // 웹 페이지의 스크립트가 그 페이지와 같은 서버에 있는 주소로만 ajax 요청을 할 수 있도록 처리하는 경우를 해제 해주는 기능
+const session = require('express-session');
+const morgan = require('morgan');
 const app = express();
 
 const database = require(__dirname + '/config/database');
@@ -18,7 +18,7 @@ app.use(
     session({
         resave: false,
         saveUninitialized: true,
-        secret: "www",
+        secret: 'www',
         cookie: {
             httpOnly: true,
             secure: false,
@@ -30,16 +30,18 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
+app.use(morgan('dev'));
 
 // routes
-app.use("/user", require("./routes/userRouter"));
+app.use('/user', require('./routes/userRouter'));
+
+// Error middleware
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.send('Error 잠시후에 다시 시도해주세요.');
+});
 
 // starting the server
 app.listen(3001, () => {
-    console.log("server started at http://localhost:3001");
+    console.log('server started at http://localhost:3001');
 });
-
-app.post("/join", (req,res)=>{
-    console.log(req.body);
-    res.json(500);
-})
